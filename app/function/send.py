e745895508973707374a4c87addb75d5827a5a19
Marotaro <77580883+Marotaro@users.noreply.github.com>
2024-01-14 22:57:43 +0100

    correction avant la version intermédiaire

--- app/function/send.py
+++ app/function/send.py
@@ -1,32 +1,28 @@
 #smtplib est une librairie rajoutée, il faut donc la téléchargée 
 import smtplib
-from app.config import MDP
+from app.config import MDP, MAIL_HOST, PORT, UTIL_MAIL
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 def send_email(to_email, message, subject):
-    #Information de connection
-    HOST = "smtp.office365.com"
-    PORT = 587
-    FROM_EMAIL = r"y.sender@outlook.com"
-
     #Information général de l'envoie
     msg = MIMEMultipart()
     msg['Subject'] = subject
-    msg['From'] = FROM_EMAIL
+    msg['From'] = UTIL_MAIL
     msg['To'] = to_email
-    msg['Cc'] = FROM_EMAIL
-    msg['Bcc'] = FROM_EMAIL
+    msg['Cc'] = UTIL_MAIL
+    msg['Bcc'] = UTIL_MAIL
 
     #Message peut être une simple chaine de caractères pour ceci il suffit de remplacer MIMEText(message,'plain')
+    #ici il s'agit dans fichier html
     msg.attach(MIMEText(message, 'html'))
     try:
-        with smtplib.SMTP(HOST, port = PORT) as smtp:
+        with smtplib.SMTP(MAIL_HOST, port = PORT) as smtp:
             #Connection au serveur smtp
             smtp.ehlo()
             smtp.starttls()
             #MDP vient du fichier config (il ne faut pas pouvoir avoir accès au mot de passe depuis github)
-            smtp.login(FROM_EMAIL, MDP)
+            smtp.login(UTIL_MAIL, MDP)
 
             # Envoie de l'email
             smtp.send_message(msg)

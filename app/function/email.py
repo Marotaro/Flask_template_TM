3f91bb7815e4994e728a7da691edb2a59d3ce25e
Marotaro <77580883+Marotaro@users.noreply.github.com>
2024-03-03 16:16:24 +0100

    mise a jour du mail

--- app/function/email.py
+++ app/function/email.py
@@ -2,12 +2,12 @@ from app.config import host
 
 def reset_message(token):
     return f"""
-    <html>
-        <body>
-            <div style="background-color: #060606; text-align: center; display: flex; flex-direction: column; align-items: center; height:auto; width: auto;">
-                <div style="text-align: center;"><h1 style="font-weight: bold; text-decoration: underline; color: #FFF; text-align: center;">Réinitialisation du mot de passe</h1></div>
-                <div style="margin-top: 100px; margin-bottom: 50px; padding: 50px; width: fit-content; height: fit-content; background-color: #424242; border-radius: 25px;"><p style="color: #FFF;"> Veuillez <a style="color: #059A6D;" href="{host}/password/reset_password/{token}">cliquer ici</a> pour réinitialiser votre mot de passe.</p></div>
-            </div>
-        </body>
-    </html>
+        <html>
+            <body>
+                <div style="background-color: #060606;">
+                    <div style=" margin-left: auto; margin-right: auto; width: 50vw;"><h1 style="font-weight: bold; text-decoration: none; color: #FFF; font: normal 800 2.5vw normal Arial,Helvetica,sans-serif; text-align: center;">Réinitialisation du mot de passe</h1></div>
+                    <div style="margin-left: auto; margin-right: auto; margin-top: 10vh; margin-bottom: 2vh; padding: 50px; width: fit-content; height: fit-content; background-color: #424242; border-radius: 25px;"><p style="color: #FFF; font: normal 800 1vw normal Arial,Helvetica,sans-serif;"> Veuillez <a style="color: #059A6D;" href="{host}/password/reset_password/{token}">cliquer ici</a> pour réinitialiser votre mot de passe.</p></div>
+                </div>
+            </body>
+        </html>
     """

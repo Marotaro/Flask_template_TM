6e87f94f8f6290b16fe88d184cec0f910d59467e
Marotaro <77580883+Marotaro@users.noreply.github.com>
2024-03-24 17:31:59 +0100

    version finale

--- app/empty-config.py
+++ app/empty-config.py
@@ -1,21 +1,25 @@
 import os
 
 #clef de chiffrement
-SECRET_KEY = ""
+SECRET_KEY = "sdg554ASDF45asdfsASF5"
 #nom de la base de donné
-DATABASE = ""
+DATABASE = "Name.db"
 
-#dossier à créer
+#dossiers
 WORKING_DIR = os.path.normpath(os.getcwd() + r"\APP\\")
+#chemin des dossiers (les dossier doivent exister)
 SHORT_FOLDER_USER = "static/image/user_icon/"
 SHORT_FOLDER_Y = "static/image/y_icon/"
 SHORT_FOLDER_POST = "static/image/user_icon/"
 
 #adresse de l'hebergeur
-host = r""
+host = r"http://127.0.00:5000"
 #informations du server de mail
-MAIL_HOST = r""
-PORT = 
+MAIL_HOST = r"smtp.office365.com"
+PORT = 587
 #informations du compte pour envoyer des mails
-UTIL_MAIL = r"" 
-MDP = r""
+UTIL_MAIL = r"exemple.exp@outlook.com" 
+MDP = r"SK.f_75!"
+
+#toutes les variables définies au dessus ne sont des exemples, il faut les compléter avec les informations que vous allez utiliser.
+#vous pouvez grader le chemin des dossiers, veillez juste à avoir les dossiers.

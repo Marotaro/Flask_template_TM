6bd2525c9fbaeee14fdf9e59afb5a984d6e41379
Marotaro <77580883+Marotaro@users.noreply.github.com>
2024-05-24 23:45:27 +0200

    change syntax for text customizations + corrections

--- app/function/text.py
+++ app/function/text.py
@@ -3,56 +3,113 @@ from markupsafe import escape
 from html_sanitizer import Sanitizer
 
 def from_custom_texte(texte):
-    # Utilisation d'une expression régulière pour trouver les balises spécifiées
-    pattern = r'{#([0-9A-Fa-f]{6})%((?:\d+\.\d+)|(?:\d+)):(.*?)}'
+
+    # remplacement couleur
+    def r_color(texte):
+        pattern = r'{#([0-9A-Fa-f]{6}):(.*?)}'
+        def remplacer(match):
+            couleur = match.group(1)
+            contenu = match.group(2)
+            return f'<span style="color: #{couleur};">{contenu}</span>'
+        
+        return re.sub(pattern, remplacer, texte)
     
-    # Fonction de remplacement pour re.sub()
-    def remplacer(match):
-        couleur = match.group(1)
-        taille = match.group(2)
-        contenu = match.group(3)
-        if float(taille) > 3:
-            taille = "3"
-        if float(taille) < 0.5:
-            taille = "0.5" 
-        return f'<span style="color: #{couleur}; font-size: calc({taille}*max(0.85vw,10px));">{contenu}</span>'
+    # remplacement taille
+    def r_size(texte):
+        pattern = r'{%((?:\d+\.\d+)|(?:\d+)):(.*?)}'
+        def remplacer(match):
+            size = match.group(1)
+            if float(size) > 3:
+                size = "3"
+            if float(size) < 0.5:
+                size = "0.5"
+            contenu = match.group(2)
+            return f'<span style="font-size: calc({size}*max(0.85vw,10px));">{contenu}</span>'
+        
+        return re.sub(pattern, remplacer, texte)
     
+    # remplacement lien
+    def r_link(texte):
+        pattern = r'{\?"(.*?)":(.*?)}'
+        def remplacer(match):
+            link = match.group(1)
+            contenu = match.group(2)
+            return f"<a href=\"{link}\">{contenu}</a>"
+        
+        return re.sub(pattern, remplacer, texte)
+
     sanitizer = Sanitizer({
-        'tags': {'madeup', 'a'},
+        'tags': {'madeup'},
         'attributes': {
             'madeup': set(),
-            'a': { 'href', 'title'}
         },
         'empty': set(),
         'separate': set(),
         'whitespace': set(),
         'keep_typographic_whitespace': True,
-        'autolink' : True       
+        'autolink' : False,    
     })
 
-    # Remplacer les balises spécifiées par les balises HTML appropriées
 
-    texte_html = re.sub(pattern, remplacer, sanitizer.sanitize(texte))
-
+    cleaned_text = texte
+    traitements = [r_color, r_size, r_link]
+    for traitement in traitements:
+        cleaned_text = traitement(cleaned_text)
     
-    return texte_html
+    auto_link = Sanitizer({
+        'tags': {'span','a'},
+        'attributes': {
+            'span': {'style'},
+            'a': {'href','title'},
+        },
+        'empty': set(),
+        'separate': set(),
+        'whitespace': set(),
+        'keep_typographic_whitespace': True,
+        'autolink' : True,
+        'is_mergeable': lambda e1, e2 :False, 
+    })
+    print(cleaned_text)
+    print(auto_link.sanitize(cleaned_text))
+    return auto_link.sanitize(cleaned_text)
+
  
-def to_custom_texte(texte_html):
-    # Expression régulière pour trouver les balises <font>
-    pattern = r'<span style="color: #([^"]*); font-size: calc\(([^*]+)\*max\(0.85vw,10px\)\);">(.*?)</span>'
+def to_custom_texte(texte):
 
-    # Fonction de remplacement pour re.sub()
-    def remplacer(match):
-        couleur = match.group(1)
-        taille = match.group(2)
-        contenu = match.group(3)
-        return "{"+f'#{couleur}%{taille}:{contenu}'+"}"
+    # remplacement couleur
+    def r_color(texte):
+        pattern = r'<span style="color: #([0-9A-Fa-f]{6});">(.*?)</span>'
+        def remplacer(match):
+            couleur = match.group(1)
+            contenu = match.group(2)
+            return '{'+f'#{couleur}:{contenu}'+'}'
+        
+        return re.sub(pattern, remplacer, texte)
     
+    # remplacement taille
+    def r_size(texte):
+        pattern =r'<span style="font-size: calc\(([^*]+)\*max\(0.85vw,10px\)\);">(.*?)</span>'
+        def remplacer(match):
+            size = match.group(1)
+            contenu = match.group(2)
+            return '{'+f'%{size}:{contenu}' +'}'
+        
+        return re.sub(pattern, remplacer, texte)
+    
+    # remplacement lien
+    def r_link(texte):
+        pattern =r'<a href="(.*?)">(.*?)</a>'
+        def remplacer(match):
+            link = match.group(1)
+            contenu = match.group(2)
+            return '{'+f'?\"{link}\":{contenu}'+'}'
+        
+        return re.sub(pattern, remplacer, texte)
+
     sanitizer = Sanitizer({
-        'tags': {'madeup', 'a'},
+        'tags': {'madeup'},
         'attributes': {
-            'madeup': set(),
-            'a': { 'href', 'title'}
+            'madeup': set()
         },
         'empty': set(),
         'separate': set(),
@@ -60,8 +117,10 @@ def to_custom_texte(texte_html):
         'keep_typographic_whitespace': True,
         'autolink' : True       
     })
-    
-    # Remplacer les balises <font> par la forme spécifiée
-    texte_converti = sanitizer.sanitize(re.sub(pattern, remplacer, texte_html))
-    
-    return texte_converti
+
+    stored_text = texte
+    traitements = [r_color, r_size, r_link]
+    for traitement in traitements:
+        stored_text = traitement(stored_text)
+
+    return sanitizer.sanitize(stored_text)
